c98c0325cb93d1f637d94df5c51cd4104bb24c27
DefineGF <34066723+DefineGF@users.noreply.github.com>
2021-01-21 12:17:35 +0800

    Update Number.cpp

--- C++/operator_overwrite/Number.cpp
+++ C++/operator_overwrite/Number.cpp
@@ -11,6 +11,8 @@ Number::~Number() {
     cout << "value = " << n <<  " destory~" << endl;
 }
 
+// const 成员函数，可以访问但不能修改成员变量
+// this -> n = this -> n + operator2.n; return *this; 为错误写法
 Number Number::operator+(const Number&operator2) const{
     Number sum;
     sum.n = this -> n + operator2.n;
