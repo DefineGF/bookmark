292e5f5b371497f4aaeb58c006523fb66622ddb3
DefineGF <34066723+DefineGF@users.noreply.github.com>
2021-01-25 15:34:54 +0800

    Create Dog.java

---
+++ Java/Dog.java
@@ -0,0 +1,10 @@
+public class Dog {
+    int foodCount;
+    public Dog(int foodCount){
+        this.foodCount = foodCount;
+    }
+    @Override
+    public String toString() {
+        return "Dog food_count = " + foodCount;
+    }
+}
